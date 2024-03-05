fa90f365074730138ce70599d91ba6557256ace4
JohnTenno <johntenno117@hotmail.com>
2024-03-04 19:32:21 -0600

    1

---
+++ first week/onlineClass/ACT 9.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int numbers[100][100];
+int size;
+int rows;
+int columns
+
+int main () {
+	
+	cout << "Digite el tamanio de la fila" << endl;
+	cin >> rows;
+	cout << "Digite el tamanio de la columna" << endl;
+	cin >> columns;
+	
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size; j ++) {
+			cout << "Ingrese un numero" << endl;
+			cin >> numbers[i][j];
+		}
+	}
+	for (int i = 0; i < size; i++) {
+		for( int j = 0; j < size; j++) {
+			cout<< i << "," << j << "-->" << numbers[i][j] << endl;
+		}
+	}
+	}
